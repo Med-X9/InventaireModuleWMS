ebefb8f9467be38aceb39898713e22576a7435d8
Mohammed BELAZZAB <93041747+Med-X9@users.noreply.github.com>
2025-07-15 11:09:51 +0100

    update

--- project/settings.py
+++ project/settings.py
@@ -106,27 +106,17 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-<<<<<<< HEAD
         'NAME': config('POSTGRES_DB'),
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
-=======
-        'NAME': config('POSTGRES_DB', default='inventairedb'),
-        'USER': config('POSTGRES_USER', default='postgres'),
-        'PASSWORD': config('POSTGRES_PASSWORD', default='root'),
-        'HOST': config('POSTGRES_HOST', default='127.0.0.1'),
-        'PORT': config('POSTGRES_PORT', default='5432'),
 
->>>>>>> dev
     }
 }
 DEFAULT_CHARSET = 'utf-8'
+
 AUTH_USER_MODEL = 'users.UserApp'
-
-
-AUTH_USER_MODEL = 'users.UserWeb'
 
 
 
@@ -402,15 +392,7 @@ JAZZMIN_UI_TWEAKS = {
 }
 
 # Security settings
-<<<<<<< HEAD
-SECURE_SSL_REDIRECT = config('DJANGO_SECURE_SSL_REDIRECT', default=True, cast=bool)
-SESSION_COOKIE_SECURE = config('DJANGO_SESSION_COOKIE_SECURE', default=True, cast=bool)
-CSRF_COOKIE_SECURE = config('DJANGO_CSRF_COOKIE_SECURE', default=True, cast=bool)
-SECURE_HSTS_SECONDS = config('SECURE_HSTS_SECONDS', default=31536000, cast=int)
-SECURE_HSTS_INCLUDE_SUBDOMAINS = config('SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True, cast=bool)
-SECURE_HSTS_PRELOAD = config('SECURE_HSTS_PRELOAD', default=True, cast=bool)
-CSRF_TRUSTED_ORIGINS = config('DJANGO_CSRF_TRUSTED_ORIGINS', cast=Csv())
-=======
+
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
@@ -418,7 +400,6 @@ CSRF_COOKIE_SECURE = False
 # SECURE_HSTS_INCLUDE_SUBDOMAINS = False
 # SECURE_HSTS_PRELOAD = False
 # CSRF_TRUSTED_ORIGINS = []
->>>>>>> dev
 
 # Email configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
