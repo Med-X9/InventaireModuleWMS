f34fdc736e5e06ff0e8b6b8a6bf006308efc139f
BoufenzF <147938378+BoufenzF@users.noreply.github.com>
2025-11-11 11:37:34 +0100

    commit 54

--- apps/mobile/urls.py
+++ apps/mobile/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
     ),
     # Gestion des assignments et jobs
     path(
-        "user/<int:user_id>/job/<int:job_id>/status/",
+        "user/<int:user_id>/assignment/<int:assignment_id>/status/",
         views.AssignmentStatusView.as_view(),
         name="mobile_assignment_status",
     ),
@@ -57,35 +57,6 @@ urlpatterns = [
     # Gestion des CountingDetail et NumeroSerie
     path(
         "job/<int:job_id>/counting-detail/",
-        views.CountingDetailView.as_view(),
-        name="mobile_counting_detail",
-    ),
-    path(
-        "user/<int:user_id>/products/",
-        views.UserProductsView.as_view(),
-        name="mobile_user_products",
-    ),
-    # Locations du même compte qu'un utilisateur
-    path(
-        "user/<int:user_id>/locations/",
-        views.UserLocationsView.as_view(),
-        name="mobile_user_locations",
-    ),
-    # Stocks du même compte qu'un utilisateur
-    path(
-        "user/<int:user_id>/stocks/",
-        views.UserStocksView.as_view(),
-        name="mobile_user_stocks",
-    ),
-    # Gestion des assignments et jobs
-    path(
-        "user/<int:user_id>/assignment/<int:assignment_id>/status/",
-        views.AssignmentStatusView.as_view(),
-        name="mobile_assignment_status",
-    ),
-    # Gestion des CountingDetail et NumeroSerie
-    path(
-        "counting-detail/",
         views.CountingDetailView.as_view(),
         name="mobile_counting_detail",
     ),
